b9ed41ac70a30e7be2ff072e3bf85c7de1c61e47
Thalison <thalissfernandes99@gmail.com>
2024-08-02 21:52:04 -0300

    Criação do router com prefixo , mudança para utilizar asyncio, criação de endpoint de teste

---
+++ mader/routers/users.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix='/users', tags=['users'])
+
+
+@router.get('/')
+async def read_users():
+    return [{'username': 'Rick'}, {'username': 'Morty'}]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from mader.app import app
 
 
-def test_read_root():
+@pytest.mark.asyncio
+async def test_read_root():
     client = TestClient(app)
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
