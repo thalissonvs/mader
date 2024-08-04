ad8de556be85dca1fb14628a898deeb4d9a3d968
Thalison <thalissfernandes99@gmail.com>
2024-08-03 23:53:28 -0300

    CRUD de usuários inicialmente finalizado, juntamente com os testes

--- mader/routers/users.py
+++ mader/routers/users.py
@@ -51,3 +51,35 @@ async def create_user(user: UserSchema, session: T_Session):
 async def read_users(session: T_Session, limit: int = 100, offset: int = 0):
     users = await session.scalars(select(User).limit(limit).offset(offset))
     return {'users': users}
+
+
+@router.delete('/{user_id}')
+async def delete_user(user_id: int, session: T_Session):
+    db_user = await session.get(User, user_id)
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+
+    session.delete(db_user)
+    await session.commit()
+    return {'message': 'User deleted'}
+
+
+@router.put('/{user_id}', response_model=UserPublic)
+async def update_user(user_id: int, user: UserSchema, session: T_Session):
+    db_user = await session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail='User not found',
+        )
+
+    db_user.username = user.username
+    db_user.email = user.email
+    db_user.password = get_password_hash(user.password)
+    await session.commit()
+    await session.refresh(db_user)
+
+    return db_user
--- tests/test_users.py
+++ tests/test_users.py
@@ -55,3 +55,61 @@ def test_get_users_with_no_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'users': []}
+
+
+def test_get_users_with_user(client, user):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+            },
+        ],
+    }
+
+
+def test_delete_user(client, user):
+    response = client.delete(f'/users/{user.id}')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_error(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        f'/users/{user.id}',
+        json={
+            'username': 'test2',
+            'email': 'test2@mail.com',
+            'password': 'password2',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': user.id,
+        'username': 'test2',
+        'email': 'test2@mail.com',
+    }
+
+
+def test_update_user_error(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'test2',
+            'email': 'test2@mail.com',
+            'password': 'password2',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
